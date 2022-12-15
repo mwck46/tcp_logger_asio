0e17780206a61126f7967a7ed476959d52770d35
mwck <mwck>
2022-12-15 06:53:42 +0000

    nonblocking queue to store log msg

--- tcp_logger_cli/tcp_logger_cli.cpp
+++ tcp_logger_cli/tcp_logger_cli.cpp
@@ -1,18 +1,25 @@
 /*****************************************************
-This project is built with visual studio 2017 build-in 
-c++ compiler (version v141), which only support 
+Point of interest:
+1) async_read_some(), async_read(), async_read_until()
+2) boost::lockfree::queue
 *****************************************************/
 
 #include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <memory>
 #include <utility>
+#include <string>
 #include <boost/asio.hpp>
 #include <boost/date_time.hpp>
 #include <boost/bind.hpp>
+#include <boost/lockfree/queue.hpp>
 
 using boost::asio::ip::tcp;
 
+
+// Only store pointer to object in boost::lockfree::queue, instead of the object directly
+boost::lockfree::queue<std::string*> _nonblockQueue(100);
 
 class session : public std::enable_shared_from_this<session>
 {
@@ -45,13 +52,21 @@ private:
   {
     if (!ec)
     {
+      // Read from asio::streambuf -> std::istream -> std::string
       std::istream is(&_streamBufr);
       std::string line;
       std::getline(is, line);
 
+      // record receiving time
       boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
       std::string clientIp = this->socket_.remote_endpoint().address().to_string();
+
+      // print to standard output
       std::printf("[%s] %s: %s\r\n", to_simple_string(timeLocal).c_str(), clientIp.c_str(), line.c_str());
+
+      snprintf(_logMsg, sizeof(_logMsg), "[%s] %s: %s", to_simple_string(timeLocal).c_str(), clientIp.c_str(), line.c_str());
+      std::string *msg = new std::string("[" + to_simple_string(timeLocal) + "] " + clientIp + ": " + line + "");
+      _nonblockQueue.push(msg);
     }
   }
 
@@ -59,10 +74,10 @@ private:
   {
     auto self(shared_from_this());
 
-    // Points to note:
-    // 1) When you call an asynchronous read or write, you need to ensure that the buffers for the operation are valid until the completion handler is called
-    //  i.e. don't use local variable
-    
+    //
+    // When you call an asynchronous read or write, you need to ensure that the buffers for the operation are valid until the completion handler is called
+    //    i.e. don't use local variable
+    //
 
     // async_read_some() 
     //    call handler immediately after receive 'some' data. In debug mode, it can read all received data, but in release mode, it can only read ~1 byte
@@ -91,6 +106,7 @@ private:
   tcp::socket socket_;
   enum { max_length = 30 };
   char data_[max_length];
+  char _logMsg[100];
 
   boost::asio::streambuf _streamBufr;
 };
@@ -98,17 +114,35 @@ private:
 class server
 {
 public:
-  server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  server(boost::asio::io_context& io_context, short port) : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
   {
     do_accept();
   }
+  
 
 private:
   void do_accept()
   {
     acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket)
     {
+
+      if (!_nonblockQueue.empty()) 
+      {
+        std::string *str;
+        std::string clientIp = socket.remote_endpoint().address().to_string();
+
+        std::ofstream ofile;
+        ofile.open(clientIp + ".txt", std::ios::app);
+        while (!_nonblockQueue.empty())
+        {
+           _nonblockQueue.pop(str);
+           //std::cout << *str << std::endl;
+           ofile << *str;
+           delete str;
+        }
+        ofile.close();
+      }
+
       if (!ec)
       {
         std::make_shared<session>(std::move(socket))->start();
